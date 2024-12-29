47f7dbeb0733cc43aacae0868d127cde67a684a6
Dileepganta <150211527+Dileepganta@users.noreply.github.com>
2024-12-29 16:04:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Banks from './pages/PublicUtilities/Banks';
 import Hospitals from './pages/PublicUtilities/Hospitals';
 import Airport from './pages/PublicUtilities/Airport';
 import BeautifulBeaches from './pages/Tourism/BeautifulBeaches';
-import {Temples } from './pages/FamousFor/Famoustemples';
+import Temples from './pages/FamousFor/Famoustemples';
 
 
 function App() {
